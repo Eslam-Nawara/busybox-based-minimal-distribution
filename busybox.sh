9bdb0859b3ad380fceb325c79032379e8a858a07
Eslam-Nawara <eslamnawara44@gmail.com>
2023-10-08 10:48:24 +0300

    refactor: use pushd and popd Instead of cd

--- busybox.sh
+++ busybox.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p src
-cd src
+pushd src
 
     echo '-- downloading linux kernel --'
     wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.15.132.tar.xz
@@ -10,10 +10,10 @@ cd src
     tar -xf linux-5.15.132.tar.xz
 
     echo  '-- compiling the kernel --'
-    cd linux-5.15.132
+    pushd linux-5.15.132
         make defconfig
         make -j8 || exit
-    cd ..
+    popd 
 
     echo '-- downloading busybox --' 
     wget https://busybox.net/downloads/busybox-1.36.1.tar.bz2
@@ -22,17 +22,17 @@ cd src
     tar -xf busybox-1.36.1.tar.bz2
     
     echo '-- building busybox in static mode --'
-    cd busybox-1.36.1
+    pushd busybox-1.36.1
         make defconfig
         echo 'CONFIG_STATIC=y' >> .config
         make -j8 || exit
-    cd ..
-cd ..
+    popd 
+popd
 
 cp src/linux-5.15.132/arch/x86_64/boot/bzImage ./
 
 mkdir -p initrd 
-cd initrd
+pushd initrd || exit
     mkdir -p bin dev proc sys
     ./../src/busybox-1.36.1/busybox --install bin
 
@@ -47,6 +47,6 @@ cd initrd
     chmod -R 777 .
 
     find . | cpio -o -H newc > ../initrd.img
-cd ..  
+popd
 
 qemu-system-x86_64 -kernel bzImage -initrd initrd.img 
